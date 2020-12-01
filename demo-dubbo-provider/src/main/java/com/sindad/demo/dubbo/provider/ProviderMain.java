bc69bb5b4073536ef926a2da19c678bc6d5e2ac9
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-12-01 22:50:34 +0800

    add rtest

--- demo-dubbo-provider/src/main/java/com/sindad/demo/dubbo/provider/ProviderMain.java
+++ demo-dubbo-provider/src/main/java/com/sindad/demo/dubbo/provider/ProviderMain.java
@@ -8,7 +8,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ProviderMain {
 
 	public static void main(String[] args) throws IOException {
-
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
 		context.start();
--- demo-dubbo-provider/src/main/java/com/sindad/demo/dubbo/provider/service/TestServiceImpl.java
+++ demo-dubbo-provider/src/main/java/com/sindad/demo/dubbo/provider/service/TestServiceImpl.java
@@ -1,14 +1,19 @@
 package com.sindad.demo.dubbo.provider.service;
 
 import java.util.Date;
-import org.springframework.stereotype.Service;
-import com.sindad.demo.dubbo.client.service.TestService;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Service;
 
 @Service
-public class TestServiceImpl implements TestService {
+@Path("/")
+public class TestServiceImpl implements com.sindad.demo.dubbo.client.service.TestService  {
 
 
+	@GET
+	@Path("/get")
 	@Override
 	public String testMethod() {
 
